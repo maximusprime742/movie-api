231aa2e156ee8e6a4429ce8c8097274471fbd5c5
Maxim Geronime <maxgeronime44@gmail.com>
2021-08-10 11:59:23 -0400

    Added serializers.

--- watchlist_app/api/urls.py
+++ watchlist_app/api/urls.py
@@ -1,5 +1,5 @@
-from django.urls import path, include
-from watchlist_app.views import movie_list, movie_details
+from django.urls import path  # , include
+from watchlist_app.api.views import movie_list, movie_details
 
 
 urlpatterns = [
