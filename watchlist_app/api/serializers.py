7aa9c2698c9c10b9fcd0bbec3154c306fe65c15b
Maxim Geronime <maxgeronime44@gmail.com>
2021-08-10 15:27:40 -0400

    Added relations.

--- watchlist_app/api/serializers.py
+++ watchlist_app/api/serializers.py
@@ -1,23 +1,17 @@
 from rest_framework import serializers
-from watchlist_app.models import Movie
+from watchlist_app.models import WatchList, StreamPlatform
 
 
-class MovieSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
-    name = serializers.CharField()
-    release_date = serializers.IntegerField()
-    description = serializers.CharField()
-    active = serializers.BooleanField()
+class WatchListSerializer(serializers.ModelSerializer):
 
-    def create(self, validated_data):
-        return Movie.objects.create(**validated_data)
+    class Meta:
+        model = WatchList
+        fields = '__all__'
 
-    def update(self, instance, validated_data):
-        instance.name = validated_data.get('name', instance.name)
-        instance.release_date = validated_data\
-                .get('release_date', instance.release_date)
-        instance.description = validated_data \
-                .get('description', instance.description)
-        instance.active = validated_data.get('active', instance.active)
-        instance.save()
-        return instance
+
+class StreamPlatformSerializer(serializers.ModelSerializer):
+    watchlist = WatchListSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = StreamPlatform
+        fields = '__all__'
--- watchlist_app/models.py
+++ watchlist_app/models.py
@@ -1,11 +1,23 @@
 from django.db import models
 
 
-class Movie(models.Model):
-    name = models.CharField(max_length=50)
-    release_date = models.IntegerField(default=0)
-    description = models.CharField(max_length=200)
-    active = models.BooleanField(default=True)
+class StreamPlatform(models.Model):
+    name = models.CharField(max_length=30)
+    about = models.CharField(max_length=150)
+    website = models.URLField(max_length=100)
 
     def __str__(self):
         return self.name
+
+
+class WatchList(models.Model):
+    title = models.CharField(max_length=50)
+    release_date = models.IntegerField(default=0)
+    description = models.CharField(max_length=200)
+    platform = models.ForeignKey(StreamPlatform, on_delete=models.CASCADE,
+                                 related_name='watchlist', default=None)
+    active = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
